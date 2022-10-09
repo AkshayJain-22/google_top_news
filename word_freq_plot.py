9975eb3c2ac391d7002bc50f299718da694ca2d3
akshay <akshay22.11.1995@gmail.com>
2022-10-09 15:09:42 +0530

    'font_size'

--- word_freq_plot.py
+++ word_freq_plot.py
@@ -8,7 +8,7 @@ def plot_generator(name,titles):
     frequency_df = frequency_df.sort_values(by=['Frequency'],ascending=False)
     frequency_df.iloc[0:20].plot(x='Words',y='Frequency',kind ='bar',figsize=(4,4))
     plt.xticks(rotation=90,)
-    plt.xlabel('xlabel', fontsize=3)
+    plt.xlabel('xlabel', fontsize=1)
     plots_filename = f'static/images/{name}_plot.png'.replace(' ','_')
     plt.savefig(plots_filename)
     return(plots_filename)
