39c76b80753ce92039e5fe7c239e03682778e755
akshay <akshay22.11.1995@gmail.com>
2022-10-09 15:26:13 +0530

    'resolution_100'

--- word_freq_plot.py
+++ word_freq_plot.py
@@ -10,5 +10,5 @@ def plot_generator(name,titles):
     plt.xticks(rotation=90,)
     plt.xlabel('xlabel', fontsize=1)
     plots_filename = f'static/images/{name}_plot.png'.replace(' ','_')
-    plt.savefig(plots_filename,dpi=300, bbox_inches='tight')
+    plt.savefig(plots_filename,dpi=100, bbox_inches='tight')
     return(plots_filename)
